f29605ace457218047940462654a739bf600ce84
Nicholas Dallas <nicholas.dallas0@gmail.com>
2020-08-27 13:35:00 -0700

    updated files for team

---
+++ db/index.js
@@ -0,0 +1,3 @@
+const Sequelize = require('sequelize')
+
+module.exports = new Sequelize('mysql://root:Javascript${}535@localhost/user_db')
---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { join } = require('path')
+
+const app = express()
+
+app.use(express.static(join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.use(require('./routes'))
+
+// force .sync(force: true) --> then remove it after you have set up path in userRoutes.js folder.
+require('./db').sync()
+  .then(() => app.listen(3000))
+  .catch(err => console.log(err))
+  
